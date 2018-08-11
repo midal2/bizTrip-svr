36c6bd8d18ce8dee805d4e5b449419bff0d2f303
midal2 <midal02@gmail.com>
2018-08-11 23:20:36 +0900

    배치DAO Sample 추가

--- src/main/java/dz/biztrip/batch/sample/ScheduleTest.java
+++ src/main/java/dz/biztrip/batch/sample/ScheduleTest.java
@@ -2,6 +2,7 @@ package dz.biztrip.batch.sample;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +14,12 @@ public class ScheduleTest {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
+    @Autowired
+    private SchduleTestDao schduleTestDao;
+
     @Scheduled(fixedDelay=1000)
     public void testSchedule(){
-        logger.debug("testSchedule : " + new Date());
+        logger.debug("testSchedule : " + schduleTestDao.getStockList());
     }
 
 }
